e086f9a1547b765d6cb93e63aa462df85334fb17
Avion19 <avienramirez@u.boisestate.edu>
2026-08-31 14:59:22 -0600

    Put Is Working

--- hw1/main.c
+++ hw1/main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "deq.h"
 
 int main() {
   Deq q=deq_new();
+  deq_head_put(q,strdup("a"));
+  deq_head_put(q,strdup("b"));
+  deq_tail_put(q,strdup("c"));
 
   char *s=deq_str(q,0);
   printf("%s\n",s);
